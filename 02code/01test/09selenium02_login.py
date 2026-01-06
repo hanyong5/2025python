e90e07bc1f252ceb8eaf0078b45e531cd3596301
hanyong5 <hanyong5@naver.com>
2026-01-06 12:31:46 +0900

    login

--- 02code/01test/09selenium02_login.py
+++ 02code/01test/09selenium02_login.py
@@ -17,8 +17,40 @@ url = "https://nid.naver.com/nidlogin.login"
 
 # selenium 설정
 options = webdriver.ChromeOptions()
+# options.add_argument('--start-maximized')
+# driver = webdriver.Chrome(options=options)
+
+
 options.add_argument('--start-maximized')
+options.add_argument('--disable-blink-features=AutomationControlled')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-gpu')
+
+options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/120.0.0.0 Safari/537.36"
+)
+
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("useAutomationExtension", False)
+
 driver = webdriver.Chrome(options=options)
+
+driver.execute_cdp_cmd(
+    "Page.addScriptToEvaluateOnNewDocument",
+    {
+        "source": """
+        Object.defineProperty(navigator, 'webdriver', {
+            get: () => undefined
+        })
+        """
+    }
+)
+
+
+
 driver.get(url)
 
 id_input = driver.find_element(By.ID,"id")
